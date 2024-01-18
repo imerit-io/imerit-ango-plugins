e9792055f754ccb9d1117d0faefd7984b3e63ec0
faruk <faruk@imerit.net>
2024-01-18 16:42:33 +0300

    cloudrun github actions

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from io import BytesIO
 from datetime import datetime
 from imerit_ango.sdk import SDK
 from imerit_ango.plugins import ExportPlugin, ExportResponse, run
+from dotenv import load_dotenv
+
+load_dotenv()
 
 HOST = os.environ['HOST']
 PLUGIN_ID = os.environ['PLUGIN_ID']
